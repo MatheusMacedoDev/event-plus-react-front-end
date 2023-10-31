1278c44063adeffd64e98c572640fdd51c522251
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-10-31 08:49:46 -0300

    Removing unused files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 import RouteView from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <RouteView/>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <RouteView/>
+  </div>
+);
+
 
 export default App;
