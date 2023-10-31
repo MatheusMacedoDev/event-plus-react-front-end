c786e040817bf78d8cb04d619315771db938d7f8
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-10-31 09:15:16 -0300

    Creating Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
 import RouteView from './routes';
+import Header from './components/Header/Header';
 
 const App = () => (
   <div className="App">
+    <Header/>
     <RouteView/>
   </div>
 );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <a href="">Home</a>
+                <a href="">Eventos</a>
+                <a href="">Tipo de Eventos</a>
+                <a href="">Login</a>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
