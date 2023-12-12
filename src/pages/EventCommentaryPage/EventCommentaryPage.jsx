83b0631c1d37684aefb665acebea947dd3bfd8ae
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-12-12 07:57:09 -0300

    Adding external repository

---
+++ src/pages/EventCommentaryPage/EventCommentaryPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EventCommentaryPage = () => {
+    return (
+        <div>
+            Página de comentários do event
+        </div>
+    );
+};
+
+export default EventCommentaryPage;
--- src/services/apiAcessor.js
+++ src/services/apiAcessor.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const apiPort = 7118;
 const localApiUrl = `https://localhost:${apiPort}/api`;
-const externalApiUrl = null;
+const externalApiUrl = 'https://event-webapi-matheus.azurewebsites.net/api';
 
 const api = axios.create({
-    baseURL: localApiUrl
+    baseURL: externalApiUrl
 });
 
 export default api;
